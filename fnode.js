511b726ca564e2ec5e46450073ea39a77f4715b4
Erik Rose <erik@mozilla.com>
2016-09-27 21:25:29 -0400

    Move Fnode to its own file.

---
+++ fnode.js
@@ -0,0 +1,79 @@
+class Fnode {
+    constructor (element) {
+        if (element === undefined) {
+            throw new Error("Someone tried to make a fnode without specifying the element they're talking about.");
+        }
+        this.element = element;
+
+        // A map of type => {score: number, note: anything}. `score` is always
+        // present and defaults to 1. A note is set iff `note` is present and
+        // not undefined.
+        this._types = new Map();
+
+        // By default, an fnode has an independent score for each of its types.
+        // However, a RHS can opt to conserve the score of an upstream type,
+        // carrying it forward into another type. To avoid runaway scores in
+        // the case that multiple rules choose to do this, we limit the
+        // contribution of an upstream type's score to being multiplied in a
+        // single time. In this set, we keep track of which upstream types'
+        // scores have already been multiplied into each type. LHS node => set of
+        // upstream types whose scores already contributed:
+        this._conservedScores = new Map();
+    }
+
+    // Return whether the given type is one of the ones attached to this node.
+    hasType (type) {
+        return this._types.has(type);
+    }
+
+    // Return our score for the given type, 1 by default.
+    getScore (type) {
+        return _typeRecordForGetting(type).score;
+    }
+
+    // Multiply one of our per-type scores by a given number. Implicitly assign
+    // us the given type.
+    multiplyScore (type, score) {
+        this._typeRecordForSetting(type).score *= score;
+    }
+
+    // Return the note for the given type, undefined if none.
+    getNote (type) {
+        return _typeRecordForGetting(type).note;
+    }
+
+    // Return whether this node has a note for the given type.
+    // Undefined is not considered a note and may be overwritten with impunity.
+    hasNote (type) {
+        return this.getNote(type) !== undefined;
+    }
+
+    // Set the note attached to one of our types. Implicitly assign us that
+    // type if we don't have it already.
+    setNote (type, note) {
+        if (note !== undefined) {
+            if (this.hasNote(type)) {
+                throw new Error(`Someone (likely the right-hand side of a rule) tried to add a note of type ${type} to an element, but one of that type already exists. Overwriting notes is not allowed, since it would make the order of rules matter.`);
+            } else {
+                this._typeRecordForSetting(type).note = note;
+            }
+        }
+    }
+
+    // Return a score/note record for a type, creating it if it doesn't exist.
+    _typeRecordForSetting (type) {
+        return setDefault(this._types, type, () => {score: 1})
+    }
+
+    // Manifest a temporary type record for reading, working around the lack of
+    // a .? operator in JS.
+    _typeRecordForGetting (type) {
+        const typeRecord = this._types.get(type);
+        return typeRecord === undefined ? {score: 1} : typeRecord;
+    }
+}
+
+
+module.exports = {
+    Fnode
+};
--- ruleset.js
+++ ruleset.js
@@ -1,5 +1,6 @@
 const {filter, forEach, map, unique} = require('wu');
 
+const {Fnode} = require('./fnode');
 const {setDefault} = require('./utils');
 const {Lhs} = require('./lhs');
 const {InwardRhs} = require('./rhs');
@@ -199,72 +200,6 @@ class OutwardRule extends Rule {
 // Construct and return the proper type of rule class based, for the moment, on
 // the inwardness/outwardness of the RHS.
 function rule(rhs, lhs) {
-}
-
-
-class Fnode {
-    constructor (element) {
-        if (element === undefined) {
-            throw new Error("Someone tried to make a fnode without specifying the element they're talking about.");
-        }
-        this.element = element;
-
-        // A map of type => {score: number, note: anything}. `score` is always
-        // present and defaults to 1. A note is set iff `note` is present and
-        // not undefined.
-        this._types = new Map();
-    }
-
-    // Return whether the given type is one of the ones attached to this node.
-    hasType (type) {
-        return this._types.has(type);
-    }
-
-    // Return our score for the given type, 1 by default.
-    getScore (type) {
-        return _typeRecordForGetting(type).score;
-    }
-
-    // Multiply one of our per-type scores by a given number. Implicitly assign
-    // us the given type.
-    multiplyScore (type, score) {
-        this._typeRecordForSetting(type).score *= score;
-    }
-
-    // Return the note for the given type, undefined if none.
-    getNote (type) {
-        return _typeRecordForGetting(type).note;
-    }
-
-    // Return whether this node has a note for the given type.
-    // Undefined is not considered a note and may be overwritten with impunity.
-    hasNote (type) {
-        return this.getNote(type) !== undefined;
-    }
-
-    // Set the note attached to one of our types. Implicitly assign us that
-    // type if we don't have it already.
-    setNote (type, note) {
-        if (note !== undefined) {
-            if (this.hasNote(type)) {
-                throw new Error(`Someone (likely the right-hand side of a rule) tried to add a note of type ${type} to an element, but one of that type already exists. Overwriting notes is not allowed, since it would make the order of rules matter.`);
-            } else {
-                this._typeRecordForSetting(type).note = note;
-            }
-        }
-    }
-
-    // Return a score/note record for a type, creating it if it doesn't exist.
-    _typeRecordForSetting (type) {
-        return setDefault(this._types, type, () => {score: 1})
-    }
-
-    // Manifest a temporary type record for reading, working around the lack of
-    // a .? operator in JS.
-    _typeRecordForGetting (type) {
-        const typeRecord = this._types.get(type);
-        return typeRecord === undefined ? {score: 1} : typeRecord;
-    }
 }
 
 
