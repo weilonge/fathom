6a877627ba4ed1afd632644f0549698a6607060a
Erik Rose <erik@mozilla.com>
2016-10-06 23:09:41 -0400

    Whoops: don't run the RHS tests exclusively.

--- test/rhs_tests.js
+++ test/rhs_tests.js
@@ -3,7 +3,7 @@ const {assert} = require('chai');
 const {func, type} = require('../index');
 
 
-describe.only('RHS', function () {
+describe('RHS', function () {
     it('Different calls combine piecewise, with rightmost repeated subfacts shadowing.', function () {
         const rhs = type('foo').score(5).func(node => ({score: 6})).asRhs();
         assert.deepEqual(rhs.fact('dummy'), {type: 'foo', score: 6});
