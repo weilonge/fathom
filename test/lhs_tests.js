04efd00d99949f99175f04d7f76d26b74dfec6d0
Erik Rose <erik@mozilla.com>
2016-10-11 16:46:27 -0400

    Add test for TypeMax LHSs.
    This brings statement coverage to 83%.

--- test/lhs_tests.js
+++ test/lhs_tests.js
@@ -1,9 +1,31 @@
-const {dom, rule, type} = require('../index');
+const {assert} = require('chai');
+const {jsdom} = require('jsdom');
+
+const {dom, rule, ruleset, out, type} = require('../index');
+
 
 describe('LHS', function () {
     it('makes a dom() LHS that rule() tolerates', function () {
         const lhs = dom('smoo');
         const rhs = type('bar');
         rule(lhs, rhs);
+    });
+
+    it('finds max-scoring nodes of a type', function () {
+        const doc = jsdom(`
+            <p></p>
+            <div></div>
+            <div></div>
+        `);
+        const rules = ruleset(
+            rule(dom('p'), type('smoo').score(2)),
+            rule(dom('div'), type('smoo').score(5)),
+            rule(type('smoo').max(), out('best'))
+        );
+        const facts = rules.against(doc);
+        const best = facts.get('best');
+        assert.equal(best.length, 2);
+        assert.equal(best[0].element.nodeName, 'DIV');
+        assert.equal(best[1].element.nodeName, 'DIV');
     });
 });
