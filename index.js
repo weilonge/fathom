ee5136fbae2079d90b5c6b9633a3c6012e2901a6
Erik Rose <erik@mozilla.com>
2017-01-26 11:48:22 -0500

    Remove "use strict". It's automatically on for ES6 modules.
    http://www.ecma-international.org/ecma-262/6.0/#sec-strict-mode-code

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-'use strict';
-
 const {rule, ruleset} = require('./ruleset');
 const {dom} = require('./lhs');
 const {out} = require('./rhs');
